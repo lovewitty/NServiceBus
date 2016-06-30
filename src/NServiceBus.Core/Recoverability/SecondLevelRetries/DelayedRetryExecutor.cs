79d7ac4fd8e885e7aebaec716e6efd3e289d711c
Daniel Marbach <daniel.marbach@particular.net>
2016-06-30 13:22:03 +0200

    Replaced IEnumerable<DeliveryConstraint> with List<DeliveryConstraint> on seam to transport

--- src/NServiceBus.Core/Recoverability/SecondLevelRetries/DelayedRetryExecutor.cs
+++ src/NServiceBus.Core/Recoverability/SecondLevelRetries/DelayedRetryExecutor.cs
@@ -29,11 +29,11 @@
             outgoingMessage.Headers[Headers.RetriesTimestamp] = DateTimeExtensions.ToWireFormattedString(DateTime.UtcNow);
 
             UnicastAddressTag messageDestination;
-            DeliveryConstraint[] deliveryConstraints = null;
+            List<DeliveryConstraint> deliveryConstraints = null;
             if (timeoutManagerAddress == null)
             {
                 // transport supports native deferred messages, directly send to input queue with delay constraint:
-                deliveryConstraints = new DeliveryConstraint[]
+                deliveryConstraints = new List<DeliveryConstraint>
                 {
                     new DelayDeliveryWith(delay)
                 };
